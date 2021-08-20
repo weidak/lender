1a83c6eadad7142392533761c4646e4e439ddb90
Tay Weida <weida.tay99@gmail.com>
2021-08-20 17:51:07 +0800

    add mechanism to track max loan amount
    create a new static variable to record max loan.
    give a getter for that

--- src/main/java/Loan.java
+++ src/main/java/Loan.java
@@ -4,11 +4,24 @@ public class Loan {
     private boolean isPaid;
     private double interestRate;
 
+    //static means its a "global" variable for all objects
+    //if changed in one object, the rest of the objects will change too
+    //not unique to one obj essentially
+
+    static private double maxLoanAmount;
+
+    public static double getMaxLoanAmount() {
+        return maxLoanAmount;
+    }
+
     public Loan(String borrower, int amount, double interestRate) {
         this.borrower = borrower;
         this.amount = amount;
         this.interestRate = interestRate;
         this.isPaid = false;
+        if (this.amount > maxLoanAmount) {
+            maxLoanAmount = this.amount;
+        }
     }
 
     public void setAsPaid() {
